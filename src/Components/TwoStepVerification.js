4a9333b47affbc3a5ada21561ff7fb4d9f7d5b27
shubhammohite753 <shubhmohite753@gmail.com>
2023-07-26 11:02:51 +0530

    otpfield

--- src/Components/TwoStepVerification.js
+++ src/Components/TwoStepVerification.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
+// import "./styles/twostep.css";
 
 
 const TwoStepVerification = () => {
-  const [otp, setOTP] = useState('');
+  // const [otp, setOTP] = useState('');
 
   const handleOTPChange = (event) => {
-    setOTP(event.target.value);
+    // setOTP(event.target.value);
   };
   const handleResetOTP = () => {
     // TODO: Implement your reset OTP logic here
     console.log('Resetting OTP...');
     // Reset the OTP value
-    setOTP('');
+    // setOTP('');
   };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
     // TODO: Handle OTP verification logic
-    console.log('OTP submitted:', otp);
+    // console.log('OTP submitted:', otp);
   };
 
   useEffect(() => {
@@ -35,7 +36,7 @@ const TwoStepVerification = () => {
 
 
   return (
-    <div id="kt_body" class="app-blank">
+    <div id="kt_body" className="app-blank">
     <div className="d-flex flex-column flex-root" id="kt_app_root">
       <div className="d-flex flex-column flex-lg-row flex-column-fluid">
         <div className="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
@@ -50,15 +51,17 @@ const TwoStepVerification = () => {
                   <div className="text-muted fw-semibold fs-5 mb-5">Enter the verification code we sent to</div>
                   <div className="fw-bold text-dark fs-3">*********753@gmail.com</div>
                 </div>
-                <div className="mb-10">
+                {/* <div className="mb-10"> */}
+                <div className='otp_input text-start mb-2'>
                   <div className="fw-bold text-start text-dark fs-6 mb-1 ms-1">Type your 6 digit security code</div>
-                  <div className="d-flex flex-wrap flex-stack">
-                    <input type="text" name="code_1" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
-                    <input type="text" name="code_2" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
-                    <input type="text" name="code_3" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
-                    <input type="text" name="code_4" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
-                    <input type="text" name="code_5" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
-                    <input type="text" name="code_6" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control bg-transparent h-60px w-60px fs-qx text-center mx-1 my-2" value="" onChange={handleOTPChange}/>
+                  {/* <div className="d-flex flex-wrap flex-stack"> */}
+                  <div className="d-flex align-items-center justify-content-between mt-2">
+                    <input type="text" name="code_1" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
+                    <input type="text" name="code_2" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
+                    <input type="text" name="code_3" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
+                    <input type="text" name="code_4" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
+                    <input type="text" name="code_5" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
+                    <input type="text" name="code_6" data-inputmask="'mask': '9', 'placeholder': ''" maxLength="1" className="form-control text-center" value="" onChange={handleOTPChange}/>
                   </div>
                 </div>
                 <div className="d-flex flex-center">
@@ -91,3 +94,26 @@ const TwoStepVerification = () => {
 };
 
 export default TwoStepVerification;
+
+
+
+
+// =================html
+{/* <div class="container height-100 d-flex justify-content-center align-items-center"> 
+<div class="position-relative"> 
+<div class="card p-2 text-center"> 
+<h6>Please enter the one time password <br/> to verify your account</h6> 
+<div> <span>A code has been sent to</span>
+ <small>*******9897</small> </div>
+ <div id="otp" class="inputs d-flex flex-row justify-content-center mt-2">
+   <input class="m-2 text-center form-control rounded" type="text" id="first" maxlength="1" /> 
+<input class="m-2 text-center form-control rounded" type="text" id="second" maxlength="1" /> 
+<input class="m-2 text-center form-control rounded" type="text" id="third" maxlength="1" /> 
+<input class="m-2 text-center form-control rounded" type="text" id="fourth" maxlength="1" /> 
+<input class="m-2 text-center form-control rounded" type="text" id="fifth" maxlength="1" />
+ <input class="m-2 text-center form-control rounded" type="text" id="sixth" maxlength="1" /> </div>
+ <div class="mt-4"> <button class="btn btn-danger px-4 validate">Validate</button> </div> </div> 
+<div class="card-2"> <div class="content d-flex justify-content-center align-items-center">
+   <span>Didn't get the code</span> 
+<a href="#" class="text-decoration-none ms-3">Resend(1/3)</a> </div> </div> </div>
+</div> */}
